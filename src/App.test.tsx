ce00b42718343ee924e2a4e4d3b748bceb795a4f
Rafael Pedicini <rafael@rafgraph.dev>
2020-10-16 22:07:30 -0400

    Add comming soon page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/v3 live detection test/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>Detect It v4 coming soon...</p>
+        <a className="App-link" href="https://detect-it-v3.rafgraph.dev">
+          v3 live detection test
         </a>
       </header>
     </div>
