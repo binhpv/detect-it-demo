5ffae0021447858b0d2ecc0f5749c9ff57fa556e
Rafael Pedicini <rafael@rafgraph.dev>
2020-10-23 09:44:39 -0400

    Upgrade stitches to v0.0.3-canary.4

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -28,3 +28,35 @@ export const darkThemeClass = css.theme({
     $blue: 'rgb(118,118,255)',
   },
 });
+
+export const globalStyles = css.global({
+  html: {
+    height: '100%',
+  },
+  body: {
+    height: '100%',
+    color: '$text',
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+    webkitFontSmoothing: 'antialiased',
+    fontSize: '16px',
+    fontWeight: 300,
+    textSizeAdjust: 'none',
+  },
+  '#root': {
+    minHeight: '100%',
+    backgroundColor: '$pageBackground',
+  },
+  'body, div, span, a, p, h1, h2, code': {
+    margin: 0,
+    border: 0,
+    padding: 0,
+    boxSizing: 'inherit',
+    font: 'inherit',
+    fontWeight: 'inherit',
+    textDecoration: 'inherit',
+    textAlign: 'inherit',
+    color: 'inherit',
+    background: 'transparent',
+  },
+});
